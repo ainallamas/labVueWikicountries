67a1c56d8c0a9b7e3b56ea58a1b80a15ead917de
Aina Llamas <ainallamas@gmail.com>
2022-10-13 18:10:01 +0200

    half lab done

---
+++ src/views/CountriesList.vue
@@ -0,0 +1,39 @@
+<template>
+<section class="section">
+
+<div class="countries-list" v-for="country in countries" :key="country.name">
+    <ul>
+    <router-link :to="{name: 'details', params: {alpha3code:country.alpha3Code}}">
+        <!-- Establecemos params para llevarnos una info con nostros que vamos a necesitar para sacar la información del país -->
+        <!-- alpha3code: -> implica que és igual a -> meterse en la array de countries -> entrar en country y después en alpha3Code -->
+        <li>{{country.name.common}}</li>
+        <li><img :src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`"></li>
+    </router-link>
+    </ul>
+</div>
+
+</section>
+</template>
+
+
+<script setup>
+
+import { ref } from 'vue'
+
+import countries from '../assets/countries.json'
+
+
+
+
+
+
+</script>
+
+
+<style scoped>
+
+.countries-list {
+    margin: 20px; 
+}
+
+</style>
---
+++ src/views/CountryDetails.vue
@@ -0,0 +1,26 @@
+<template>
+
+<!-- router view -> queremos insertar un componente que se lo vamos a definir en el .js -->
+<!-- router link -> se utiliza como una etiqueta <a> y te lleva a otra página. -->
+
+
+
+
+<!--Desde CountriesList nos hemos traído la información del alpha3Code -->
+
+</template>
+
+<script setup>
+
+// Aquí ponemos useRoute -> para poder importar.
+// Esto significa que este elemento cuando se le llame estará recibiendo un parámetro. 
+// Es una etiqueta del vue-router -> ¡SIEMPRE ES ASÍ!
+import { useRoute } from 'vue-router';
+
+
+
+
+</script>
+
+<style scoped>
+</style>
